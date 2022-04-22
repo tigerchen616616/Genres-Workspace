3483d023322760a39e32369436e5b9039fad7f83
Yining Chen <74680846+tigerchen616616@users.noreply.github.com>
2022-04-22 14:32:48 -0700

    New Fullstack Project Initialized on Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "genres": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
